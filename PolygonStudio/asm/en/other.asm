42363c66c2324466b15d85755b7aab7e943775ca
LuigiBlood <luigiblood@gmail.com>
2021-12-21 21:42:12 +0100

    Update some messages and add japanese text for other.asm

--- PolygonStudio/asm/en/other.asm
+++ PolygonStudio/asm/en/other.asm
@@ -3,119 +3,113 @@
 seek(0x11F980)
 base 0x800AC4C0
 error_loading:
-putTextASCIInoSeek("Loading...")                       //800AC4C0
+//ロード中です
+textASCII0("Loading...")                       //800AC4C0
 error_saving:
-putTextASCIInoSeek("Saving...")
+//セーブ中です
+textASCII0("Saving...")
 error_diskerror:
-putTextASCIInoSeek("Disk Error")
+//ディスクエラー
+textASCII0("Disk Error")
 error_eject:
-putTextASCIInoSeek("Please eject the disk.")
+//ディスクを取り出してください。
+textASCII0("Please eject the disk.")
 error_insert:
-putTextASCIInoSeek("Please insert the disk.")
+//ディスクを差し込んでください。
+textASCII0("Please insert the disk.")
 
 error_wrongdisk1:
-putTextASCIInoSeek("The wrong disk may")
+//間違ったディスクが差し込まれ
+textASCII0("The wrong disk may")
 error_wrongdisk2:
-putTextASCIInoSeek("have been inserted.")
+//ている可能性があります。
+textASCII0("have been inserted.")
 error_wrongdisk3:
-putTextASCIInoSeek("Please insert")
+//正しいディスクに入れかえて
+textASCII0("Please insert")
 error_wrongdisk4:
-putTextASCIInoSeek("the correct disk.")
+textASCII0("the correct disk.")
 
 error_removeddisk1:
-putTextASCIInoSeek("When the power switch of the")  //800AC588
+//ＮＩＮＴＥＮＤＯ６４本体の
+textASCII0("When the power switch of the")  //800AC588
 error_removeddisk2:
-putTextASCIInoSeek("NINTENDO 64 Control Deck is switched on,")  //800AC5A4
+//電源スイッチを入れたときの
+textASCII0("NINTENDO 64 Control Deck is switched on,")  //800AC5A4
 error_removeddisk3:
-putTextASCIInoSeek("Polygon Studio")  //800AC5C0
+//「ポリゴンスタジオ」の
+textASCII0("Polygon Studio")  //800AC5C0
 error_readmanual:
-putTextASCIInoSeek("Please read the instruction manual.")  //800AC5D8
+//取扱説明書をお読みください。
+textASCII0("Please read the instruction manual.")  //800AC5D8
 
 error_note1:
-putTextASCIInoSeek("Note: While the ACCESS light is blinking,")  //800AC5F8
+//注意：アクセスランプ点滅中に
+textASCII0("Note: While the ACCESS light is blinking,")  //800AC5F8
 error_note2:
-putTextASCIInoSeek("please do not eject the disk.")  //800AC618
+//ディスクを抜かないでください。
+textASCII0("please do not eject the disk.")  //800AC618
 
 error_readmanual2:
-putTextASCIInoSeek("For more details, please read the manual.")  //800AC638
+//詳しくは、取扱説明書をお読み
+textASCII0("For more details, please read the manual.")  //800AC638
 
 error_please:
-putTextASCIInoSeek(" ")  //800AC658
+//ください。
+textASCII0(" ")  //800AC658
 
 error_insertagain:
 error_removeddisk4:
-putTextASCIInoSeek("Please insert the disk again.")  //800AC664
+//ディスクを差し込みなおして
+textASCII0("Please insert the disk again.")  //800AC664
 
 //Change hardcoded pointers
 //Loading
-seek(0xDF416)
-dh error_loading
+seek(0xDF416); dh error_loading
 
 //Saving
-seek(0xDF42A)
-//dh error_saving
+seek(0xDF42A); //dh error_saving
 
-seek(0xDF43E)
-//dh error_diskerror
+seek(0xDF43E); //dh error_diskerror
 
-seek(0xDF452)
-//dh error_eject
+seek(0xDF452); //dh error_eject
 
-seek(0xDF466)
-//dh error_insert
+seek(0xDF466); //dh error_insert
 
-seek(0xDF47A)
-//dh error_wrongdisk1
+seek(0xDF47A); //dh error_wrongdisk1
 
 //Disk Error
-seek(0xDFF7E)
-dh error_diskerror
+seek(0xDFF7E); dh error_diskerror
 
 //Removed disk
-seek(0xE0152)
-dh error_removeddisk1
-seek(0xE017E)
-dh error_removeddisk2
-seek(0xE01AA)
-dh error_removeddisk3
-seek(0xE01D6)
-dh error_removeddisk4
+seek(0xE0152); dh error_removeddisk1
+seek(0xE017E); dh error_removeddisk2
+seek(0xE01AA); dh error_removeddisk3
+seek(0xE01D6); dh error_removeddisk4
 
 //Wrong disk
-seek(0xE020A)
-dh error_wrongdisk1
-seek(0xE0236)
-dh error_wrongdisk2
-seek(0xE0262)
-dh error_wrongdisk3
-seek(0xE028E)
-dh error_wrongdisk4
+seek(0xE020A); dh error_wrongdisk1
+seek(0xE0236); dh error_wrongdisk2
+seek(0xE0262); dh error_wrongdisk3
+seek(0xE028E); dh error_wrongdisk4
 
 //Please read the instruction manual.
-seek(0xE02CA)
-dh error_readmanual
+seek(0xE02CA); dh error_readmanual
 
 //Note
-seek(0xE0306)
-dh error_note1
-seek(0xE3332)
-dh error_note2
+seek(0xE0306); dh error_note1
+seek(0xE3332); dh error_note2
 
 //Please read for more info
-seek(0xE035E)
-dh error_readmanual2
-seek(0xE038A)
-dh error_please
+seek(0xE035E); dh error_readmanual2
+seek(0xE038A); dh error_please
 
 //Insert Disk
-seek(0xE03B6)
-dh error_insert
+seek(0xE03B6); dh error_insert
 
 //Insert Disk again
-seek(0xE03F2)
-dh error_insertagain
-seek(0xE041E)
-dh error_please
+seek(0xE03F2); dh error_insertagain
+seek(0xE041E); dh error_please
 
 
 
@@ -125,77 +119,104 @@ seek(0x1C9978)
 base 0x802FAD10
 //4 lines "Do you want to return to the main menu after the automatic save?" (???)
 other_autosavereturn1:
-  putTextASCIInoSeek("Do you want to return")
+//オートセーブしてから
+  textASCII0("Do you want to return")
 other_autosavereturn2:
-  putTextASCIInoSeek("to the main menu")
+//セレクトがめんに
+  textASCII0("to the main menu")
 other_autosavereturn3:
-  putTextASCIInoSeek("after the automatic save?")
+//もどりますが
+  textASCII0("after the automatic save?")
 other_autosavereturn4:
-  putTextASCIInoSeek("")
+//よろしいですか？
+  textASCII0("")
 
 //3 lines "The painting will disappear. Are you sure?"
 other_paintgone1:
-  putTextASCIInoSeek("The painting will")
+//さくひんのペイントが
+  textASCII0("The paint job")
 other_paintgone2:
-  putTextASCIInoSeek("disappear.")
+//消えますが
+  textASCII0("will disappear.")
 other_paintgone3:
-  putTextASCIInoSeek("Are you sure?")
+//よろしいですか？
+  textASCII0("Are you sure?")
 
 //3 lines "The stamp will disappear. Are you sure?"
 other_stampgone1:
-  putTextASCIInoSeek("The stamp will")
+//オリジナルスタンプが
+  textASCII0("The stamp will")
 other_stampgone2:
-  putTextASCIInoSeek("disappear.")
+//消えますが
+  textASCII0("disappear.")
 other_stampgone3:
-  putTextASCIInoSeek("Are you sure?")
+//よろしいですか？
+  textASCII0("Are you sure?")
 
-//2 lines "Initialization of the automatic save area of the disk."
+//6 lines "Initialization of the automatic save area of the disk. Auto-saved work will disappear. Are you sure?" (L+Down+B)
+//Hold L + Down D-Pad + B from startup
 other_initauto1:
-  putTextASCIInoSeek("Initialization of the")
+//ディスクの自動セーブ領域を
+  textASCII0("This will initialize the")
 other_initauto2:
-  putTextASCIInoSeek("automatic save area.")
-
-//4 lines "Auto-saved work will disappear. Are you sure?" (L+Down+B)
-other_photosgone1:
-  putTextASCIInoSeek("Auto-saved content")
-other_photosgone2:
-  putTextASCIInoSeek("will disappear.")
-other_photosgone3:
-  putTextASCIInoSeek("")
-other_photosgone4:
-  putTextASCIInoSeek("Are you sure?")
+//初期化します。
+  textASCII0("auto-save area of the disk.")
+other_initauto3:
+//自動セーブされた
+  textASCII0("Automatically saved")
+other_initauto4:
+//さくひんなどが
+  textASCII0("creations and progress")
+other_initauto5:
+//消えてしまいますが
+  textASCII0("will be lost.")
+other_initauto6:
+//よろしいですか？
+  textASCII0("Are you sure?")
 
 //6 lines "Initialization of the data area of the disk. All the saved data will disappear. Are you sure?" (L+Down+A)
+//Hold L + Down D-Pad + A from startup
 other_initdisk1:
-  putTextASCIInoSeek("Initialization of the")
+//ディスクのデータ領域を
+  textASCII0("This will initialize")
 other_initdisk2:
-  putTextASCIInoSeek("data area of the disk.")
+//初期化します。
+  textASCII0("the data of the disk.")
 other_initdisk3:
-  putTextASCIInoSeek("All your saved content")
+//すべてのさくひんが
+  textASCII0("All saved creations")
 other_initdisk4:
-  putTextASCIInoSeek("will disappear.")
+//消えてしまいますが
+  textASCII0("will be lost.")
 other_initdisk5:
-  putTextASCIInoSeek("Are you sure?")
+//よろしいですか？
+  textASCII0("Are you sure?")
 
 //2 lines "Initialization... Do not eject the disk."
 other_initprogress1:
-  putTextASCIInoSeek("Initialization...")
+//初期化しています。
+  textASCII0("Initialization...")
 other_initprogress2:
-  putTextASCIInoSeek("Do not eject the disk.")
+//ディスクを抜かないでください。
+  textASCII0("Please do not eject the disk.")
 
 //3 lines "Your current work will disappear. Are you sure?"
 other_currentgone1:
-  putTextASCIInoSeek("Your current work")
+//げんざいのさくひんが
+  textASCII0("Your current creation")
 other_currentgone2:
-  putTextASCIInoSeek("will disappear.")
+//消えますが
+  textASCII0("will be lost.")
 other_currentgone3:
-  putTextASCIInoSeek("Are you sure?")
+//よろしいですか？
+  textASCII0("Are you sure?")
 
 seek(0x1C9708)
+//RAM 802FAAA0
 dw other_autosavereturn1, other_autosavereturn2, other_autosavereturn3, other_autosavereturn4, 0, 0
 dw other_paintgone1, other_paintgone2, other_paintgone3, 0, 0, 0
 dw other_stampgone1, other_stampgone2, other_stampgone3, 0, 0, 0
-dw other_initauto1, other_initauto2, other_photosgone1, other_photosgone2, other_photosgone3, other_photosgone4
+dw other_initauto1, other_initauto2, other_initauto3, other_initauto4, other_initauto5, other_initauto6
 dw other_initdisk1, other_initdisk2, other_initdisk3, other_initdisk4, other_initdisk5, 0
 dw other_initprogress1, other_initprogress2, 0, 0, 0, 0
 dw other_currentgone1, other_currentgone2, other_currentgone3, 0, 0, 0
@@ -203,41 +224,51 @@ dw other_currentgone1, other_currentgone2, other_currentgone3, 0, 0, 0
 
 //Default Container Names
 seek(0xE3EAC)
+//ブロックさくひん (81A1)
 putTextASCIInoSeeknoEnd("Block Work")
 dh 0x81A1
 db 0x00
 
 seek(0xE3EC0)
+//３Ｄさくひん (86A3)
 putTextASCIInoSeeknoEnd("3D Work")
 dh 0x86A3
 db 0x00
 
 seek(0xE3ED0)
+//ステ－ジさくひん (819C)
 putTextASCIInoSeeknoEnd("Stage Work")
 dh 0x819C
 db 0x00
 
 seek(0xE3EE4)
+//２Ｄさくひん (86A6)
 putTextASCIInoSeeknoEnd("2D Work")
 dh 0x86A6
 db 0x00
 
 seek(0xE3EF4)
+//どうりょくマシン (86A4)
 putTextASCIInoSeeknoEnd("Power Machine")
 dh 0x86A4
 db 0x00
 
 seek(0xE3F08)
-putTextASCIInoSeek("Container 1")
+//いれもの１
+textASCII0("Container 1")
 
 seek(0xE3F14)
-putTextASCIInoSeek("Container 2")
+//いれもの２
+textASCII0("Container 2")
 
 seek(0xE3F20)
-putTextASCIInoSeek("Container 3")
+//いれもの３
+textASCII0("Container 3")
 
 seek(0xE3F2C)
-putTextASCIInoSeek("Container 4")
+//いれもの４
+textASCII0("Container 4")
 
 seek(0xE3F38)
-putTextASCIInoSeek("Container 5")
+//いれもの５
+textASCII0("Container 5")
