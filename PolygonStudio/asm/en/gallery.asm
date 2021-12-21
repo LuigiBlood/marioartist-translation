b09302569207d4996f0e3ba74bb693571c93104e
LuigiBlood <luigiblood@gmail.com>
2021-12-21 22:54:44 +0100

    Add japanese translation to Gallery text

--- PolygonStudio/asm/en/gallery.asm
+++ PolygonStudio/asm/en/gallery.asm
@@ -1,14 +1,25 @@
 //Mario Artist Polygon Studio
 //Gallery
 seek(0x732A04)
+//エントリーNo.1
 textRegular(24, "Entry No.1")
+//エントリーNo.2
 textRegular(24, "Entry No.2")
+//エントリーNo.3
 textRegular(24, "Entry No.3")
+//エントリーNo.4
 textRegular(24, "Entry No.4")
+//エントリーNo.5
 textRegular(24, "Entry No.5")
+//エントリーNo.6
 textRegular(24, "Entry No.6")
+//エントリーNo.7
 textRegular(24, "Entry No.7")
+//エントリーNo.8
 textRegular(24, "Entry No.8")
+//エントリーNo.9
 textRegular(24, "Entry No.9")
+//エントリーNo.10
 textRegular(24, "Entry No.10")
+//セレクトがめんにもどる
 textRegular(24, "Back to Main Menu")
