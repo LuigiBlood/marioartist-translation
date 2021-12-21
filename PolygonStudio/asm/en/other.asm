e6a891936dd37aed9e8ddf681fac7d5cc2ddd984
LuigiBlood <luigiblood@gmail.com>
2021-12-21 22:43:33 +0100

    Translate Polygon Studio Credits

--- PolygonStudio/asm/en/other.asm
+++ PolygonStudio/asm/en/other.asm
@@ -272,3 +272,227 @@ textASCII0("Container 4")
 seek(0xE3F38)
 //いれもの５
 textASCII0("Container 5")
+
+
+//Credits
+seek(0x27B868)
+base 0x807B6790
+credits_title1:
+//RAM 807B6790 - Mario Artist Polygon Studio
+textASCII0("Mario Artist Polygon Studio")
+credits_name0:
+//RAM 807B67AC - Space
+textASCII0(" ")
+
+credits_title2:
+//RAM 807B67B0 - Producer
+textASCII0("Producer")
+credits_name1:
+//RAM 807B67BC - 沢野  貴夫
+textASCII0("Takao Sawano")
+
+credits_title3:
+//RAM 807B67C8 - Director
+textASCII0("Director")
+credits_name2:
+//RAM 807B67D4 - 松岡　洋史
+textASCII0("Hirofumi Matsuoka")
+
+credits_title4:
+//RAM 807B67E0 - Designers
+textASCII0("Designers")
+credits_name3:
+//RAM 807B67EC - 藤井　英樹
+textASCII0("Hideki Fujii")
+credits_name4:
+//RAM 807B67F8 - 桑原　浩安
+textASCII0("Hiroyasu Kuwabara")
+credits_name5:
+//RAM 807B6804 - 大西　洋
+textASCII0("Yo Ohnishi")
+credits_name6:
+//RAM 807B6810 - 鎌田　真男
+textASCII0("Masao Kamada")
+credits_name7:
+//RAM 807B681C - 大坪　大剛
+textASCII0("Hirotake Ohtsubo")
+
+credits_title5:
+//RAM 807B6828 - Programmers
+textASCII0("Programmers")
+credits_name8:
+//RAM 807B6834 - 西山  尚哉
+textASCII0("Naoya Nishiyama")
+credits_name9:
+//RAM 807B6840 - 河本　浩一
+textASCII0("Kouichi Kawamoto")
+credits_name10:
+//RAM 807B684C - 矢田　裕人
+textASCII0("Hiroto Yada")
+credits_name11:
+//RAM 807B6858 - 太田  敬三
+textASCII0("Keizo Ohta")
+credits_name12:
+//RAM 807B6864 - 西村　克仁
+textASCII0("Katsuhito Nishimura")
+
+credits_title6:
+//RAM 807B6870 - Modeling System Programmers
+textASCII0("Modeling System Programmers")
+credits_name13:
+//RAM 807B688C - Hillel Rom
+textASCII0("Hillel Rom")
+credits_name14:
+//RAM 807B6898 - Chris Cederwall
+textASCII0("Chris Cederwall")
+credits_name15:
+//RAM 807B68A8 - Larry Malone
+textASCII0("Larry Malone")
+credits_name16:
+//RAM 807B68B8 - Luca Pisati
+textASCII0("Luca Pisati")
+
+credits_title7:
+//RAM 807B68C4 - Sound Composer
+textASCII0("Sound Composer")
+credits_name17:
+//RAM 807B68D4 - 戸高　一生
+textASCII0("Kazumi Totaka")
+
+credits_title8:
+//RAM 807B68E0 - Sound Programmer
+textASCII0("Sound Programmer")
+credits_name18:
+//RAM 807B68F4 - 井田　康
+textASCII0("Yasushi Ida")
+
+credits_title9:
+//RAM 807B6900 - Technical Support
+textASCII0("Technical Support")
+credits_name19:
+//RAM 807B6914 - 安田　芳人
+textASCII0("Yoshito Yasuda")
+
+credits_title10:
+//RAM 807B6920 - Special Thanks
+textASCII0("Special Thanks")
+credits_name20:
+//RAM 807B6930 - 渋谷  明夫
+textASCII0("Akio Shibuya")
+credits_name21:
+//RAM 807B693C - 松浦　謙二
+textASCII0("Kenji Matsuura")
+credits_name22:
+//RAM 807B6948 - Space
+textASCII0(" ")
+credits_name23:
+//RAM 807B694C - 朝妻  尚子
+textASCII0("Naoko Asazuma")
+credits_name24:
+//RAM 807B6958 - 内田  暁子
+textASCII0("Akiko Uchida")
+credits_name25:
+//RAM 807B6964 - 北村  香織
+textASCII0("Kaori Kitamura")
+credits_name26:
+//RAM 807B6970 -   小島  しのぶ
+textASCII0("Shinobu Kojima")
+credits_name27:
+//RAM 807B6980 - 古市  健次
+textASCII0("Kenji Furuichi")
+credits_name28:
+//RAM 807B698C - 山岸  亮  
+textASCII0("Ryo Yamagishi")
+credits_name29:
+//RAM 807B6998 - 渡辺  丈紘
+textASCII0("Takao Watanabe")
+credits_name30:
+//RAM 807B69A4 - Space
+textASCII0(" ")
+credits_name31:
+//RAM 807B69A8 - スーパーマリオクラブ
+textASCII0("Super Mario Club")
+
+seek(0x27AFA0)  //remap
+credits_title11:
+//RAM 807B69C0 - Supervisor
+textASCII0("Supervisor")
+credits_name32:
+//RAM 807B69CC - 宮本　茂
+textASCII0("Shigeru Miyamoto")
+credits_title12:
+//RAM 807B69D8 - Executive Producer
+textASCII0("Executive Producer")
+credits_name33:
+//RAM 807B69EC - 山内　　
+textASCII0("Hiroshi Yamauchi")
+credits_name34:
+//RAM 807B69F8 - Nichimen Graphics
+textASCII0("Nichimen Graphics")
+
+//RAM 807B6324 - (c)Nintendo
+
+seek(0x27B408)
+dw 0x5,credits_title1,0
+dw 0x6,0,0 //dw 0x0,credits_name0,0
+dw 0x6,0,0
+dw 0x4,credits_title2,0
+dw 0x2,credits_name1,0
+dw 0x6,0,0
+dw 0x4,credits_title3,0
+dw 0x2,credits_name2,0
+dw 0x6,0,0
+dw 0x4,credits_title4,0
+dw 0x2,credits_name3,0
+dw 0x2,credits_name4,0
+dw 0x2,credits_name5,0
+dw 0x2,credits_name6,0
+dw 0x2,credits_name7,0
+dw 0x6,0,0
+dw 0x4,credits_title5,0
+dw 0x2,credits_name8,0
+dw 0x2,credits_name9,0
+dw 0x2,credits_name10,0
+dw 0x2,credits_name11,0
+dw 0x2,credits_name12,0
+dw 0x6,0,0
+dw 0x4,credits_title6,0
+dw 0x2,credits_name13,0
+dw 0x2,credits_name14,0
+dw 0x2,credits_name15,0
+dw 0x2,credits_name16,0
+dw 0x6,0,0
+dw 0x4,credits_title7,0
+dw 0x2,credits_name17,0
+dw 0x6,0,0
+dw 0x4,credits_title8,0
+dw 0x2,credits_name18,0
+dw 0x6,0,0
+dw 0x4,credits_title9,0
+dw 0x2,credits_name19,0
+dw 0x6,0,0
+dw 0x4,credits_title10,0
+dw 0x2,credits_name20,0
+dw 0x2,credits_name21,0
+dw 0x6,0,0 //dw 0x0,credits_name22,0
+dw 0x2,credits_name23,0
+dw 0x2,credits_name24,0
+dw 0x2,credits_name25,0
+dw 0x2,credits_name26,0
+dw 0x2,credits_name27,0
+dw 0x2,credits_name28,0
+dw 0x2,credits_name29,0
+dw 0x6,0,0 //dw 0x0,credits_name30,0
+dw 0x2,credits_name31,0
+dw 0x6,0,0
+dw 0x4,credits_title11,0
+dw 0x2,credits_name32,0
+dw 0x6,0,0
+dw 0x4,credits_title12,0
+dw 0x2,credits_name33,0 //dw 0x3,credits_name33,0
+dw 0x6,0,0
+dw 0x6,0,0
+dw 0x6,0,0
+dw 0x1,0x807B6324,0
+dw 0x1,credits_name34,0
+dw 0x7,0,0
