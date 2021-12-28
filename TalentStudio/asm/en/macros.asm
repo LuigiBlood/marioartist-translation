ae03a1e2f778dcb1d0e0434f0127567f7a919708
LuigiBlood <luigiblood@gmail.com>
2021-12-28 20:47:45 +0100

    Clean up Talent Studio macros

--- TalentStudio/asm/en/macros.asm
+++ TalentStudio/asm/en/macros.asm
@@ -1,5 +1,5 @@
 //Common Macros
-//NEW
+//VERY NEW
 macro seek(n) {
   origin {n} - seek_diff
 }
@@ -9,7 +9,7 @@ macro seekFile(n) {
 }
 
 macro check(n) {
-  if origin() - seek_diff > {n} {
+  if (origin() > {n} - seek_diff) {
     error "Too much space taken."
   }
 }
@@ -20,6 +20,66 @@ macro checkFile(n) {
   }
 }
 
+macro skip(size) {
+  variable skipSeek = origin()+{size}
+  origin skipSeek
+}
+
+macro outputFile(filename) {
+  output {filename}
+  origin 0
+  base 0x0A000000
+}
+
+macro textASCII(size, text) {
+  variable skipSeek = origin()+{size}
+  textASCII0({text})
+  origin skipSeek
+}
+
+macro textASCII0(text) {
+  ASCIIMap()
+  db {text}
+  db 0x00
+}
+
+macro textASCIIE(text) {
+  ASCIIMap()
+  db {text}
+}
+
+macro textRegular(size, text) {
+  variable skipSeek = origin()+{size}
+  textRegular0({text})
+  origin skipSeek
+}
+
+macro textRegular0(text) {
+  RegularMap()
+  db {text}
+  db 0x00
+}
+
+macro textRegularA(char, text) {
+  RegularMap()
+  db {char}, " "
+  db {text}
+  db " ", {char}
+  db 0x00
+}
+
+macro textSJIS0(text) {
+  ShiftJISMap()
+  dh {text}
+  dh 0x0000
+}
+
+macro textSJISE(text) {
+  ShiftJISMap()
+  dh {text}
+}
+
+//NEW
 macro textEntryH(size, text) {
   variable skipSeek = origin()+{size}
   textEntryH0({text})
@@ -88,193 +148,11 @@ macro textEntryASCIIBox(n, text, symbol, text2) {
   dh 0x0000
 }
 
-macro skip(size) {
-  variable skipSeek = origin()+{size}
-  origin skipSeek
-}
-
-macro outputFile(filename) {
-  output {filename}
-  origin 0
-  base 0x0A000000
-}
-
 //OLD
-macro putText(n, text) {
-  seek({n})
-  putTextnoSeek({text})
-}
-
-macro putTextnoSeek(text) {
-  putTextnoSeeknoEnd({text})
-  db 0x0000
-}
-
-macro putTextnoSeeknoEnd(text) {
-  RegularMap()
-  db {text}
-}
-
-macro putText2(n, text) {
-  seek({n})
-  putText2noSeek({text})
-}
-
-macro putText2noSeek(text) {
-  RegularMap()
-  dh 0x5000
-  db {text}
-  db 0x0000
-}
-
-macro putText3(n, pre, text) {
-  seek({n})
-  putText3noSeek({pre}, {text})
-}
-
-macro putText3noSeek(pre, text) {
-  putText3noSeeknoEnd({pre}, {text})
-  db 0x0000
-}
-
-macro putText3noSeeknoEnd(pre, text) {
-  RegularMap()
-  db {pre}
-  db {text}
-}
-
-macro putText4(n, pre, text, post) {
-  seek({n})
-  putText4noSeek({pre}, {text}, {post})
-}
-
-macro putText4noSeek(pre, text, post) {
-  RegularMap()
-  db {pre}
-  db {text}
-  db {post}
-  db 0x0000
-}
-
-macro putTextSJIS(n, text) {
-  seek({n})
-  putTextSJISnoSeek({text})
-}
-
-macro putTextSJISnoSeek(text) {
-  ShiftJISMap()
-  dh 0x5400, 0xFFFF
-  dh {text}
-  dh 0x0000
-}
-
-macro putTextSJIS2(n, text, text2) {
-  seek({n})
-  ShiftJISMap()
-  dh 0x5400, 0xFFFF
-  dh {text}
-  db 0x0A
-  dh {text2}
-  dh 0x0000
-}
-
-macro putTextSJIS3(n, text, text2, text3) {
-  seek({n})
-  putTextSJIS3noSeek({text}, {text2}, {text3})
-}
-
-macro putTextSJIS3noSeek(text, text2, text3) {
-  ShiftJISMap()
-  dh 0x5400, 0xFFFF
-  dh {text}
-  db 0x0A
-  dh {text2}
-  db 0x0A
-  dh {text3}
-  dh 0x0000
-}
-
-macro putTextSJIS4(n, text, text2, text3, text4) {
-  seek({n})
-  putTextSJIS4noSeek({text}, {text2}, {text3}, {text4})
-}
-
-macro putTextSJIS4noSeek(text, text2, text3, text4) {
-  ShiftJISMap()
-  dh 0x5400, 0xFFFF
-  dh {text}
-  db 0x0A
-  dh {text2}
-  db 0x0A
-  dh {text3}
-  db 0x0A
-  dh {text4}
-  dh 0x0000
-}
-
-macro putTextSJIS5(n, text, text2, text3, text4, text5) {
-  seek({n})
-  putTextSJIS5noSeek({text}, {text2}, {text3}, {text4}, {text5})
-}
-
-macro putTextSJIS5noSeek(text, text2, text3, text4, text5) {
-  ShiftJISMap()
-  dh 0x5400, 0xFFFF
-  dh {text}
-  db 0x0A
-  dh {text2}
-  db 0x0A
-  dh {text3}
-  db 0x0A
-  dh {text4}
-  db 0x0A
-  dh {text5}
-  dh 0x0000
-}
-
-macro putTextSJIS7noSeek(text, text2, text3, text4, text5, text6, text7) {
-  ShiftJISMap()
-  dh 0x5400, 0xFFFF
-  dh {text}
-  db 0x0A
-  dh {text2}
-  db 0x0A
-  dh {text3}
-  db 0x0A
-  dh {text4}
-  db 0x0A
-  dh {text5}
-  db 0x0A
-  dh {text6}
-  db 0x0A
-  dh {text7}
-  dh 0x0000
-}
-
 macro putTextASCII(n, text) {
   ASCIIMap()
   seek({n})
   db {text}, 0
-}
-
-macro putTextASCIInoEnd(n, text) {
-  ASCIIMap()
-  seek({n})
-  db {text}
-}
-
-macro putTextASCIInoSeek(text) {
-  ASCIIMap()
-  db {text}, 0
-}
-
-macro putTextASCIIBox(n, text, symbol, text2) {
-  ASCIIMap()
-  seek({n})
-  db {text}
-  dh {symbol}
-  db {text2}
-  dh 0x0000
 }
 
 // Character Table
